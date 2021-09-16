4981583a6769cebe25a0b6ea9ae5cad1730c53c2
h7ml <h7ml@qq.com>
2021-09-16 12:47:48 +0800

    mixin

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Bus from './bus'
 import store from './store'
+import MqttMixin from './mixins/mqtt'
 Vue.use(Bus)
 Vue.config.productionTip = false
-
+Vue.mixin(MqttMixin)
 new Vue({
   store,
   render: h => h(App),
